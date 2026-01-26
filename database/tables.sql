13ecd47594f4a0952a490c5f8406732c8f1968a9
Ayushkumar Maurya <ayushskmaurya@gmail.com>
2026-01-26 21:35:38 +0530

    Created tables for misc transactions.

--- database/tables.sql
+++ database/tables.sql
@@ -59,3 +59,29 @@ CREATE TABLE mutual_fund_transactions (
     FOREIGN KEY (mutual_fund_id) REFERENCES mutual_funds(id) ON DELETE CASCADE,
     CHECK (deposit IS NOT NULL OR withdrawal IS NOT NULL)
 );
+
+/*---------------------------------------------------------
+Miscellaneous Transactions
+---------------------------------------------------------*/
+CREATE TABLE `groups` (
+    id INT AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    description VARCHAR(200),
+    PRIMARY KEY (id),
+    UNIQUE (name)
+);
+
+CREATE TABLE misc_transactions (
+    id INT AUTO_INCREMENT,
+    group_id INT NOT NULL,
+    date DATE NOT NULL DEFAULT (CURRENT_DATE),
+    description VARCHAR(100),
+    deposit DECIMAL(10, 2),
+    withdrawal DECIMAL(10, 2),
+    remark VARCHAR(200),
+    verified BOOLEAN NOT NULL DEFAULT 0,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id),
+    FOREIGN KEY (group_id) REFERENCES `groups`(id) ON DELETE CASCADE,
+    CHECK (deposit IS NOT NULL OR withdrawal IS NOT NULL)
+);
