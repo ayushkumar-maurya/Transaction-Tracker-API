0e357b870bb922b2ac51ecd06256d837a819c48c
Ayushkumar Maurya <ayushskmaurya@gmail.com>
2026-01-26 20:50:33 +0530

    Created mutual fund related tables.

--- database/tables.sql
+++ database/tables.sql
@@ -1,6 +1,10 @@
+/*---------------------------------------------------------
+Bank Transactions
+---------------------------------------------------------*/
 CREATE TABLE banks (
     id INT AUTO_INCREMENT,
-    name VARCHAR(100) NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    description VARCHAR(200),
     PRIMARY KEY (id),
     UNIQUE (name)
 );
@@ -12,10 +16,46 @@ CREATE TABLE bank_transactions (
     description VARCHAR(100),
     deposit DECIMAL(10, 2),
     withdrawal DECIMAL(10, 2),
-    remark TEXT,
+    remark VARCHAR(200),
     verified BOOLEAN NOT NULL DEFAULT 0,
     created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (id),
     FOREIGN KEY (bank_id) REFERENCES banks(id) ON DELETE CASCADE,
+    CHECK (deposit IS NOT NULL OR withdrawal IS NOT NULL)
+);
+
+/*---------------------------------------------------------
+Mutual Fund Transactions
+---------------------------------------------------------*/
+CREATE TABLE brokerages (
+    id INT AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    description VARCHAR(200),
+    PRIMARY KEY (id),
+    UNIQUE (name)
+);
+
+CREATE TABLE mutual_funds (
+    id INT AUTO_INCREMENT,
+    brokerage_id INT NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    description VARCHAR(200),
+    PRIMARY KEY (id),
+    FOREIGN KEY (brokerage_id) REFERENCES brokerages(id) ON DELETE CASCADE,
+    UNIQUE (brokerage_id, name)
+);
+
+CREATE TABLE mutual_fund_transactions (
+    id INT AUTO_INCREMENT,
+    mutual_fund_id INT NOT NULL,
+    date DATE NOT NULL DEFAULT (CURRENT_DATE),
+    description VARCHAR(100),
+    deposit DECIMAL(10, 2),
+    withdrawal DECIMAL(10, 2),
+    remark VARCHAR(200),
+    verified BOOLEAN NOT NULL DEFAULT 0,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id),
+    FOREIGN KEY (mutual_fund_id) REFERENCES mutual_funds(id) ON DELETE CASCADE,
     CHECK (deposit IS NOT NULL OR withdrawal IS NOT NULL)
 );
